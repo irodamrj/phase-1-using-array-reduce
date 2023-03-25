84baf874f3429d3f758d3346c6d8d591b9d30edf
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-25 13:00:57 +0300

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+function reducer(){
+    return batteryBatches.reduce(function(batteries,acc){
+        return batteries+acc;
+    }, 0)
+}
+
 // Code your solution here
+let totalBatteries = reducer();
+console.log(totalBatteries);
